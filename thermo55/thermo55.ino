f9769a699f73fae20c388efddbe14e4c66285e94
topquark22 <topquark22@yahoo.ca>
2023-11-19 14:45:51 -0500

    support threshold

--- thermo55/thermo55.ino
+++ thermo55/thermo55.ino
@@ -1,4 +1,5 @@
 #include "Adafruit_MAX31855.h"
+#include "LiquidCrystal.h"
 
 const int PIN_OUT = 2;
 const int PIN_OUT_ = 3;
@@ -10,9 +11,30 @@ const int thermoCLK = 13; // SPI serial clock
 
 Adafruit_MAX31855 thermocouple(thermoCLK, thermoCS, thermoDO);
 
-const int BAUD_RATE = 9600;
+// LCD1602 pins
+const int PIN_RS = 7;
+const int PIN_E = 6;
+const int PIN_DS4 = A1;
+const int PIN_DS3 = A2;
+const int PIN_DS2 = A3;
+const int PIN_DS1 = A4;
 
-float WARNING_THRESHOLD_C = 24; // TODO read this from nRLF24L radio
+LiquidCrystal lcd(PIN_RS, PIN_E, PIN_DS4, PIN_DS3, PIN_DS2, PIN_DS1);
+
+const int PIN_THRESHOLD = A0;
+
+// alarm threshold range in degrees C
+const float TEMP_LOW = -20;
+const float TEMP_HIGH = 200;
+
+float alarmTemperature() {
+  int reading = analogRead(PIN_THRESHOLD);
+  // interpolate
+  float alarmTemp = TEMP_LOW + (TEMP_HIGH - TEMP_LOW) * reading / 1023;
+  return alarmTemp;
+}
+
+const int BAUD_RATE = 9600;
 
 void setup() {
 
@@ -20,6 +42,7 @@ void setup() {
   pinMode(PIN_OUT_, OUTPUT);
   digitalWrite(PIN_OUT,LOW);
   digitalWrite(PIN_OUT_, HIGH);
+  pinMode(PIN_THRESHOLD, INPUT);
   
   Serial.begin(BAUD_RATE);
   Serial.println("MAX31855 test");
@@ -45,9 +68,12 @@ void loop() {
       Serial.println("Short to VCC!");
     }
   } else {
+    float threshold = alarmTemperature();
+    Serial.print("Alarm threshold: ");
+    Serial.println(threshold);
     Serial.print("Celsius: ");
     Serial.println(c);
-    if (c >= WARNING_THRESHOLD_C) {
+    if (c >= threshold) {
       digitalWrite(PIN_OUT, HIGH);
       digitalWrite(PIN_OUT_, LOW);
     } else {
